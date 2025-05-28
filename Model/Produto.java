8c16214ea30758da89599cadd33a543a41aa4368
Rebecca Beccari Fagundes <164184762+sallybato@users.noreply.github.com>
2025-05-27 21:08:34 -0300

    Create Produto.java

---
+++ Model/Produto.java
@@ -0,0 +1,27 @@
+public abstract class  Produto {
+
+    private String nome;
+    private double preco;
+    private String descricao;
+
+    public Produto(String nome,double preco, String descricao) {
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+   public abstract String descricaoDetalhada(); //cada produto precisa ter sua propria descrição detalhada
+
+}
