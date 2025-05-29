f92156debf6414b317fe8cd5b663abbe70b6f34a
Rebecca Beccari Fagundes <164184762+sallybato@users.noreply.github.com>
2025-05-29 20:38:07 -0300

    Update Cardapio.java

--- Model/Cardapio.java
+++ Model/Cardapio.java
@@ -1,4 +1,4 @@
-package Model;
+package model;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +15,12 @@ public void cadastrarProdutos(){
         produtos.add(new Comidas("Pão de Queijo", 10.0f,
                 "pãozinho assado, feito à base de polvilho azedo e queijo",
                 "nenhum","1 unidade (grande)"));
-        produtos.add(new Comidas("Empadão de Frango",12.50f))
+        produtos.add(new Comidas("Empadão de Frango",12.50f,
+                "Crocante por fora e molhadinho por dentro, este empadão de frango leva cebola, " +
+                        "requeijão, milho verde, tomate, cheiro-verde e azeitonas no recheio.",
+                "maionese extra","1 fatia (grande)"));
+        produtos.add(new Comidas("Folhado 4 Queijos",9.50f,"folhado quentinho e macio com recheio" +
+                " de queijo mussarela, parmesão, provolone e gorgonzola.", "nenhum","1 unidade (médio)"));
 
 }
     public Cardapio(List<Produto> produtos) {
