46f8a0c5aa9cd92949beb5cb51f3b7d8b5f44de4
Rebecca Beccari Fagundes <164184762+sallybato@users.noreply.github.com>
2025-05-29 11:23:15 -0300

    Update Cardapio.java

--- Model/Cardapio.java
+++ Model/Cardapio.java
@@ -1,1 +1,24 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cardapio {
+
+    private List<Produto> produtos;
+
+    public Cardapio(){
+        this.produtos = new ArrayList<>();
+        cadastrarProdutos();
+    }
+public void cadastrarProdutos(){
+        produtos.add(new Bebidas("Espresso Doppio",9.90f,"Espresso com leite e um toque de avelã," +
+                " finalizado com chantilly e calda de chocolate.","pequeno","demasiado quente"));
+
+}
+    public Cardapio(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+}
 
