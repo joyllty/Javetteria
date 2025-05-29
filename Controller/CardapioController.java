e021385d9f104d2ee37baa1a2565783ba6fea5c3
Rebecca Beccari Fagundes <164184762+sallybato@users.noreply.github.com>
2025-05-29 11:24:30 -0300

    Update CardapioController.java

--- Controller/CardapioController.java
+++ Controller/CardapioController.java
@@ -1,1 +1,10 @@
+package Controller;
+import Model.Cardapio;
+import Model.Produto;
+import View.CardapioView;
+public class CardapioController {
+    private Cardapio cardapio;
+    private CardapioView view;
 
+
+}
