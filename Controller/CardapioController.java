f0f9e6586a1ca1ca04e4f721d933d7328a48a718
Rebecca Beccari Fagundes <164184762+sallybato@users.noreply.github.com>
2025-06-05 11:34:25 -0300

    Update CardapioController.java

--- Controller/CardapioController.java
+++ Controller/CardapioController.java
@@ -2,9 +2,22 @@ package controller;
 import model.Cardapio;
 import model.Produto;
 import view.CardapioView;
+
+import java.util.List;
+
 public class CardapioController {
     private Cardapio cardapio;
-    private CardapioView view;
 
+
+    public CardapioController(){
+       this.cardapio = new Cardapio();
+    } // construtor
+    public List<Produto> listarProdutos(){
+        return cardapio.getProdutos();
+        //metodo para retornar todos os produtos
+    }
+//metodos como exibircardapiocompleto
+    // buscarporcategoria
+    //buscarpornome
 
 }
