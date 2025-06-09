c46a87f0f8c724bf3824e58808af1a46a71a5eba
Bruna Chapelin <bruna.polvina@gmail.com>
2025-06-09 01:20:21 -0300

    Implementação interface Menu

--- src/View/MenuFuncionario.java
+++ src/View/MenuFuncionario.java
@@ -1,10 +1,11 @@
 package view;
 
+import model.Menu;
 import utils.InputHelper;
 
 import java.util.Scanner;
 
-public class MenuFuncionario {
+public class MenuFuncionario implements Menu {
     //#---------------- CORES ----------------#
     public static final String LAVENDER = "\u001B[38;5;183m";
     public static final String CREME = "\u001B[38;5;229m";
@@ -12,41 +13,43 @@ public class MenuFuncionario {
     public static final String RESET = "\u001B[0m";
     //---------------------------#--------------------------//
 
-    public static void menuFuncionario(){
-        int op;
-
+    // função principal do menu funcionário - sobrescrita
+    @Override
+    public void executar() {
+        int opFuncionario = 0;
         do {
-            System.out.println("\n ╔══════════════════════════╗");
-            System.out.println(" ║                          ║");
-            System.out.println(" ║ [1] REGISTRAR PEDIDOS    ║");
-            System.out.println(" ║ [2] ACOMPANHAR PEDIDOS   ║");
-            System.out.println(" ║ [3] PAGAMENTO            ║");
-            System.out.println(" ║ [4] VOLTAR               ║");
-            System.out.println(" ║                          ║");
-            System.out.println(" ╚══════════════════════════╝ ");
+            menuFuncionario();
+            opFuncionario= InputHelper.lerInt();
+            verificarOp(opFuncionario);
 
-            System.out.print(
-                    "\n" + (LAVENDER + ">>" + RESET) + CREME + " Selecione uma opção: ");
-            op = InputHelper.lerInt();
+        } while (opFuncionario != 4);
+    }
 
-            switch(op){
-                case 1:
-                    System.out.println("Registrando pedido......");
+    // ler opção do menu funcionário
+    public void verificarOp(int op){
 
-                    break;
-                case 2:
-                    System.out.println("Exibir fila de pedidos.....");
-                    break;
-                case 3:
-                    System.out.println("[1] CARTÃO\n[2] DINHEIRO\n[3]PIX");
-                    break;
-                case 4:
-                    System.out.println("\nVoltando...");
-                    break;
-                default:
-                    System.out.print("\n" + (LAVENDER + ">>" + RESET) + CREME + " Opção inválida!");
-            }
+        switch (op) {
+            case 1 -> System.out.println("Registrando pedido......");
+            case 2 -> System.out.println("Exibir fila de pedidos.....");
+            case 3 -> System.out.println("[1] CARTÃO\n[2] DINHEIRO\n[3]PIX");
+            case 4 -> System.out.println("\nVoltando...");
+            default -> System.out.print("\n" + (LAVENDER + ">>" + RESET) + CREME + "Opção inválida!");
+        }
+    }
 
-        } while (op != 4);
+    // view menu funcionario
+    public static void menuFuncionario(){
+        System.out.println("\n ╔══════════════════════════╗");
+        System.out.println(" ║                          ║");
+        System.out.println(" ║ [1] REGISTRAR PEDIDOS    ║");
+        System.out.println(" ║ [2] ACOMPANHAR PEDIDOS   ║");
+        System.out.println(" ║ [3] PAGAMENTO            ║");
+        System.out.println(" ║ [4] VOLTAR               ║");
+        System.out.println(" ║                          ║");
+        System.out.println(" ╚══════════════════════════╝ ");
+
+        System.out.print(
+                "\n" + (LAVENDER + ">>" + RESET) + CREME + " Selecione uma opção: ");
+
     }
 }
