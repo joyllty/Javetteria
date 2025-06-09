22f9e403ad5623fdd2d4f11a75ff55847b0ed768
Bruna Chapelin <161771692+joyllty@users.noreply.github.com>
2025-06-09 01:07:49 -0300

    Update MenuGerente.java

--- src/View/MenuGerente.java
+++ src/View/MenuGerente.java
@@ -4,12 +4,11 @@ import model.Menu;
 import utils.InputHelper;
 
 public class MenuGerente implements Menu {
+    //#---------------- CORES ----------------#
     public static final String LAVENDER = "\u001B[38;5;183m";
     public static final String CREME = "\u001B[38;5;229m";
     public static final String BROWN = "\u001B[38;5;130m";
     public static final String RESET = "\u001B[0m";
-
-
     //---------------------------#--------------------------//
 
     // função principal do menu gerência - sobrescrita
